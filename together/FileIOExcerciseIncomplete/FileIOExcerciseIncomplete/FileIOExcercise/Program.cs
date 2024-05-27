be7ce33680fe8e141a4ac5cfc81bbdec836689d1
progsen <>
2024-05-27 08:35:18 +0200

    added together with class practice

---
+++ together/FileIOExcerciseIncomplete/FileIOExcerciseIncomplete/FileIOExcercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = ????;//maak een nieuwe naar 1 van de directories in de test directory (volledig path vergeet je @ niet!)
+            foreach (DirectoryInfo sub in ???)//haal alle directories op die in dir zitten!
+            {
+                Console.Write(sub.Name[5]);
+            }
+            Console.WriteLine();
+            //welke boodschap krijg je?
+            Console.ReadLine();
+        }
+    }
+}
